6fbceae9768753f869c1cc7649ccb7e183b7829e
Kumodoushin <p.goralski@10g.pl>
2020-11-12 17:50:41 +0100

    small controller to receive list of episodes

---
+++ SW.Api/Controllers/EpisodeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SW.Model;
+
+namespace SW.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EpisodeController:ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Episode.List);
+        }
+    }
+}
